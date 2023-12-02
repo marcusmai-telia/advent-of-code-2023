0f2b9a6b1d0a2923453bf1f2a93ce1e4883fe908
marcusmai-telia <110463987+ngg846@users.noreply.github.com>
2023-12-02 17:07:09 +0100

    day3_prep

---
+++ day03/run-part1.js
@@ -0,0 +1,14 @@
+const { log } = require('console');
+let fs = require('fs');
+const line = fs.readFileSync('./day02/input.txt', 'utf-8').split(/\r?\n/);
+
+let value = 0;
+
+// Check every game (line)
+line.forEach((element) => {
+
+
+});
+
+
+console.log('Part 1: ' + value);

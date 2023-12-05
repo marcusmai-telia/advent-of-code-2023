2045b0b87975944a7a14c9708b7b8c68ab035746
marcusmai-telia <110463987+ngg846@users.noreply.github.com>
2023-12-05 08:06:57 +0100

    Day4 done

--- day04/run-part2.js
+++ day04/run-part2.js
@@ -5,6 +5,8 @@ const lines = fs.readFileSync('./day04/input.txt', 'utf-8').split(/\r?\n/);
 let totalPoints = 0;
 let winCards = [];
 let myCards = [];
+let cards = [];
+let totaltScoreCards = 0;
 
 //Function to return the values found i both arrays
 function intersect(a, b) {
@@ -54,28 +56,73 @@ lines.forEach((element) => {
 for (let r = 0; r < lines.length; r++) {
   const winNo = winCards[r];
   const myNo = myCards[r];
-  console.log('Winning: '+winNo);
-  console.log('My Numbers: '+myNo);
+  // console.log('Winning: '+winNo);
+  // console.log('My Numbers: '+myNo);
 
   // Calculation of the score for card (row)
   myWinNo = intersect(winNo, myNo)
-  console.log('Numbers found: ' + myWinNo);
+  // console.log('Numbers found: ' + myWinNo);
   if (myWinNo.length === 1) {
     totalPoints += 1;
-    console.log('Points: 1');
+    // console.log('Points: 1');
+    cards.push([1, 1]);
   }
   else if (myWinNo.length > 1) {
     totalPoints += 2 ** (myWinNo.length - 1);
-    console.log('Points: ' + 2 ** (myWinNo.length - 1));
+    // console.log('Points: ' + 2 ** (myWinNo.length - 1));
+    cards.push([1, myWinNo.length]);
   }
   else {
     totalPoints += 0;
-    console.log('Points: 0');
+    // console.log('Points: 0');
+    cards.push([1, 0]);
   }
-  console.log('------------');
+  // console.log('------------');
 }
 
-console.log('Part 1: ' + totalPoints);
+//Print the cards, rounds and copies
+// cards.forEach(element => {
+//   console.log(element);
+// });
+// console.log('----------------');
+
+// Every card type 1 / 2 / 3
+for (let l = 0;  l < cards.length; l++) {
+
+  // console.log(cards[l]);
+  const card = cards[l];
+  
+  const rounds = cards[l][0];
+  const copies = cards[l][1];
+
+  // console.log(rounds);
+  // console.log(copies);
+
+  // Run the amount of rounds
+  for (let r = 0; r < rounds; r++) {
+
+    //Copy the amout of copies down
+    for (let c = 0; c < copies; c++) {
+
+      cards[l + 1 + c][0] += 1;
+    }
+
+  }
+  
+};
+
+//Print the cards, rounds and copies
+// console.log('----------------');
+// cards.forEach(element => {
+//   console.log(element);
+// });
+// console.log('----------------');
+
+cards.forEach(element => {
+  totaltScoreCards += parseInt(element[0]);
+});
+
+console.log('Part 1: ' + totaltScoreCards);
 
 
 
